de7e44e5cb32648e0ce285d3603dac821480ad3a
macOS <macos@macOSs-MacBook-Pro.local>
2021-12-02 22:36:01 +0700

    ADD makefile all in one

---
+++ user-interface/main_camera.py
@@ -0,0 +1,46 @@
+import socketio
+import cv2
+import base64
+import time
+import json
+
+
+sio = socketio.Client()
+
+def read_json():
+    f = open('package.json') # Opening JSON file
+    data = json.load(f) # returns JSON object as a dictionary
+    url = data['config']['myUrl'] # list
+    f.close() # Closing file
+    return url 
+
+url = read_json()
+
+@sio.event
+def connect():
+    print("I'm connected!")
+
+@sio.event
+def connect_error():
+    print("The connection failed!")
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+try:
+    sio.connect(url+':5000/')
+except:
+  print("Server error connection")
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+while(True):
+    x  =  1
+    ret, img = vid.read()                     # get frame from webcam
+    res, frame = cv2.imencode('.jpg', img,[cv2.IMWRITE_JPEG_QUALITY,80])    # from image to binary buffer
+    data = base64.b64encode(frame)              # convert to base64 format
+    try:
+        sio.emit('videoVision', data)                      # send to server
+    except:
+        x = 1
