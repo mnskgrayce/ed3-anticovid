96f6cc78febdd5528fe3852fa039ce311b9d6475
DuctrinhDt <ngocducad1997@gmail.com>
2021-12-03 05:19:23 +0700

    CHANGE time

--- qr-camera/grove.py/combinecode.py
+++ qr-camera/grove.py/combinecode.py
@@ -6,7 +6,7 @@ import time
 import RPi.GPIO as GPIO1
 from datetime import datetime
 from seeed_dht import DHT                                                   # Temperature and humidity sensor library
-# from grove.display.jhd1802 import JHD1802
+from grove.display.jhd1802 import JHD1802
 from mraa import getGpioLookup
 from grove.grove_mini_pir_motion_sensor import GroveMiniPIRMotionSensor     # Motion sensor library
 from grove.grove_moisture_sensor import GroveMoistureSensor                 # Moisture sensor library
@@ -27,10 +27,6 @@ import requests
 import socketio
 import base64
 
-GPIO1.setwarnings(False)
-GPIO1.setmode(GPIO1.BCM)
-GPIO1.setup(17, GPIO1.IN)         #Read output from PIR motion sensor
-
 # Socket io 
 sio = socketio.Client()
 @sio.event
@@ -44,16 +40,11 @@ def disconnect():
     print('disconnected from server')
 sio.connect('http://192.168.50.46:4000/')                                   # Connect through Pi IP port 4000
 
-# Function detecton on
-def on_motion():
-    i=GPIO1.input(17)
-    if i==0:                 #When output from motion sensor is LOW
-        print ("No intruders",i)
-        time.sleep(0.1)
-    elif i==1:               #When output from motion sensor is HIGH
-        print ("Intruder detected",i)
-        on_detect()
-        time.sleep(0.1)
+# Define for PIR motion sensor
+GPIO1.setwarnings(False)
+GPIO1.setmode(GPIO1.BCM)
+GPIO1.setup(17, GPIO1.IN)         #Read output from PIR motion sensor
+
 # Funtion for PWM of relay used for buzzer
 class GroveRelay(GPIO):
     def __init__(self, pin):
@@ -70,7 +61,7 @@ motionSensor = GroveMiniPIRMotionSensor(5)
 # Grove - Moisture Sensor connected to port A0
 moisture_sensor = GroveMoistureSensor(0)
 # Grove - 16x2 LCD(White on Blue) connected to I2C port
-# lcd = JHD1802()
+lcd = JHD1802()
 # Grove - Temperature&Humidity Sensor connected to port D22
 temp_sensor = DHT('11', 22)
 # Grove - LED Button connected to port D16
@@ -102,7 +93,7 @@ myData = None
 # Constant variable
 TIME_QR = 150                                                              # Limit time for scaning QR
 DISTANCE = 50                                                              # limit distance for ultrasonic measure
-TIME_MOTION = 50                                                           # Limit time for motion
+TIME_MOTION = 4                                                           # Limit time for motion
 
 language ="en"  
 dataFile_path = r"/home/pi/Documents/Hardware/grove.py/myDataFile.txt"     # storing data check in
@@ -114,6 +105,79 @@ def check_in_list(name):
     file_path = r"/home/pi/Documents/Hardware/grove.py/check_in_list.txt"  # people are allowed
     with open(file_path, "a") as f:                                        # Open and read file
         f.write(name+"\n")
+
+# Funtion for reading temperature and humidity
+def roomCondition():
+    global humi
+    global temp
+    global mois  
+    while True:
+        humi, temp = temp_sensor.read()                                        # read temperture and humidity data from sensor
+        mois = moisture_sensor.moisture
+        sio.emit('sensor', {'temperature':temp, 'humidity':humi,'moisture':mois})
+        time.sleep(1)
+
+#Funtion for getting room condition
+def LCD_roomCondition():
+    global system_state                                                    # In/Out checking state
+    global humi
+    global temp
+    global mois
+    global total
+    global exiting
+    global entering
+    global fps
+    global lcd_state
+    if lcd_state == 0:                                                  # Room condition state
+        lcd.clear()
+        # write temp and humi to lcd
+        lcd.setCursor(0, 0)                                   
+        lcd.write('Waiting data')                                  # Write temperture to lcd
+    elif lcd_state == 1:                                                # QR valid scan state
+        lcd.clear()
+        lcd.setCursor(0, 0)                                   
+        lcd.write('QR scan valid')
+        lcd.setCursor(1, 0)                                   
+        lcd.write('Enter the Room')
+        time.sleep(5)
+        lcd_state = 0
+        system_state = 0
+    elif lcd_state == 2:                                                # Room full state
+        lcd.clear()
+        lcd.setCursor(0, 0)                                   
+        lcd.write('room is full')
+        lcd.setCursor(1, 0)                                   
+        lcd.write('Enter rejected')
+        time.sleep(5)
+        lcd_state = 0
+    elif lcd_state == 3:                                                # QR invalid scan state
+        lcd.clear()
+        lcd.setCursor(0, 0)                                   
+        lcd.write('QR scan invalid')
+        lcd.setCursor(1, 0)                                   
+        lcd.write('Enter rejected')
+        time.sleep(5)
+        if lcd_state == 1:
+            lcd_state = 1
+        else:
+            lcd_state = 0
+    elif lcd_state == 4:                                                # Exit state
+        lcd.clear()
+        lcd.setCursor(0, 0)                                   
+        lcd.write('Exit the room')
+        time.sleep(5)
+        lcd_state = 0
+        system_state = 0 
+    elif lcd_state == 5:                                                # No QR scan state
+        lcd.clear()
+        lcd.setCursor(0, 0)                                   
+        lcd.write('No QR scan')
+        time.sleep(5)
+        lcd_state = 0
+
+    # Sending data to UI through Socket IO  
+    sio.emit('motion', {'total_people':total,  'people_in':entering, 'people_out':exiting})
+    sio.emit('checkout',system_state)
 
 # Funtion for buzzer QR valid, buzzing 2 times
 def buzzer_valid():
@@ -144,15 +208,6 @@ def buzzer_timeout():
     buzzer.on()
     time.sleep(1)
     buzzer.off()
-    
-# Funtion for reading temperature and humidity
-def roomCondition():
-    global humi
-    global temp
-    global mois  
-    humi, temp = temp_sensor.read()                                        # read temperture and humidity data from sensor
-    mois = moisture_sensor.moisture
-    time.sleep(1)
 
 # Funtion for blinking led in invalid scan
 def led_blink():
@@ -164,75 +219,6 @@ def led_blink():
     time.sleep(0.5)
     button.led.light(False)
 
-# Funtion for getting room condition
-# def LCD_roomCondition():
-#     global system_state                                                    # In/Out checking state
-#     global humi
-#     global temp
-#     global mois
-#     global total
-#     global exiting
-#     global entering
-#     global fps
-#     global lcd_state
-#     if lcd_state == 0:                                                  # Room condition state
-#         lcd.clear()
-#         # write temp and humi to lcd
-#         lcd.setCursor(0, 0)                                   
-#         lcd.write('Tem:{}C'.format(temp))                                  # Write temperture to lcd
-#         lcd.setCursor(1, 0)
-#         lcd.write('Hum:{}%'.format(humi))                                  # Write humididy to lcd
-#         lcd.setCursor(0, 8)
-#         lcd.write('Moi:{}%'.format(mois))                                  # Write moisture to lcd      
-#         lcd.setCursor(1, 8)                                                # Write total people in room
-#         lcd.write('total:{}'.format(total))
-#     elif lcd_state == 1:                                                # QR valid scan state
-#         lcd.clear()
-#         lcd.setCursor(0, 0)                                   
-#         lcd.write('QR scan valid')
-#         lcd.setCursor(1, 0)                                   
-#         lcd.write('Enter the Room')
-#         time.sleep(5)
-#         lcd_state = 0
-#         system_state = 0
-#     elif lcd_state == 2:                                                # Room full state
-#         lcd.clear()
-#         lcd.setCursor(0, 0)                                   
-#         lcd.write('room is full')
-#         lcd.setCursor(1, 0)                                   
-#         lcd.write('Enter rejected')
-#         time.sleep(5)
-#         lcd_state = 0
-#     elif lcd_state == 3:                                                # QR invalid scan state
-#         lcd.clear()
-#         lcd.setCursor(0, 0)                                   
-#         lcd.write('QR scan invalid')
-#         lcd.setCursor(1, 0)                                   
-#         lcd.write('Enter rejected')
-#         time.sleep(5)
-#         if lcd_state == 1:
-#             lcd_state = 1
-#         else:
-#             lcd_state = 0
-#     elif lcd_state == 4:                                                # Exit state
-#         lcd.clear()
-#         lcd.setCursor(0, 0)                                   
-#         lcd.write('Exit the room')
-#         time.sleep(5)
-#         lcd_state = 0
-#         system_state = 0 
-#     elif lcd_state == 5:                                                # No QR scan state
-#         lcd.clear()
-#         lcd.setCursor(0, 0)                                   
-#         lcd.write('No QR scan')
-#         time.sleep(5)
-#         lcd_state = 0
-
-    # Sending data to UI through Socket IO
-    sio.emit('sensor', {'temperature':temp, 'humidity':humi,'moisture':mois})  
-    sio.emit('motion', {'total_people':total,  'people_in':entering, 'people_out':exiting})
-    sio.emit('checkout',system_state)
-    
 # Function for notify room is full
 def roomfull_on():
     # Notify room full by turning on off LED
@@ -241,11 +227,87 @@ def roomfull_on():
     if total < 5:
         button.led.light(False)
 
-# Funtion for socket io
-def threadingforQR(img):
-    res, frame = cv2.imencode('.jpg', img,[cv2.IMWRITE_JPEG_QUALITY,80])   # from image to binary buffer
-    data = base64.b64encode(frame)                                         # convert to base64 format
-    sio.emit('video', data)                                                # send to server
+# Function detecton on
+def on_motion():
+    i=GPIO1.input(17)
+    if i==1:               #When output from motion sensor is HIGH
+        print ("Intruder detected",i)
+        on_detect()
+        time.sleep(1)
+
+# Function for Distance dectect
+def measure():
+    distance_detect = distance.get_distance()
+    print('{} cm'.format(distance_detect))
+    return distance_detect
+
+
+# Funtion for motion sensor dectected
+def on_detect():
+    global time_motion 
+    global flag                                                             # global variable
+    global count_QR
+    global exiting
+    global total
+    global entering
+    global system_state
+    global distant_count                                                    
+    global lcd_state
+    global current_distance
+    global pre_distance
+    if system_state == 3:                                                   # Invalid
+        system_state = 0
+        print('Invalidout')
+    elif system_state == 2:                                                 # Room full
+        system_state = 0
+        print('Room full')
+    elif system_state == 5:                                                 # No scan
+        system_state = 0
+        print('No scan')    
+    elif system_state == 0 or system_state == 1 or system_state == 4:
+        flag = 1
+        system_state = 0
+        sio.emit('checkout',system_state)
+        # turn on Ultra sonic sensor
+        while flag != 0:
+            distance_detect = measure()
+            if current_distance == 1:
+                pre_distance += current_distance
+            elif current_distance == 0:
+                pre_distance = 0
+            print('pre_distance', pre_distance)
+            # print('cur_distance', current_distance)
+            time.sleep(0.2)
+            if distance_detect < DISTANCE:                                 # if people in range -> run QR scan
+                current_distance = 1
+                distant_count += 1
+                if pre_distance == 1:
+                    time_motion = 0
+                if pre_distance == 4:
+                    distant_count = 0
+                    pre_distance = 0
+                    time_motion = 0
+                    QRcheck()
+                    flag = 0
+            else:
+                current_distance = 0                                                           # in case people exit the room
+                time_motion += 1                                            # counting time for open ultra sonic
+                if time_motion == TIME_MOTION:                              # Time limit for run ultra sonic
+                    time_motion = 0
+                    if total == 0:
+                        exiting = exiting
+                    else:
+                        exiting += 1
+                        system_state = 4  
+                        lcd_state = 4
+                    flag = 0
+        total = entering - exiting                                          # total people in room
+        roomfull_on()
+        sio.emit('motion', {'total_people':total,  'people_in':entering, 'people_out':exiting})  # Send data inside room to UI
+        sio.emit('checkout',system_state)
+        t4 = threading.Thread(target=LCD_roomCondition)
+        t4.start()
+        count_QR = 0                                                        # Set time count back 0 for next loop
 
 # Funtion for scaning QR
 def QRcheck():
@@ -326,10 +388,10 @@ def QRcheck():
         total = entering - exiting                                          # total people in room
         sio.emit('checkout',system_state)
         sio.emit('fps_qr', fps)                                             # Send fps to UI
-        # print('Number of people in room: ', total)
-        # print('People in: ', entering)
-        # print('People out: ', exiting)
-        # print('count: ', count_QR)
+        print('Number of people in room: ', total)
+        print('People in: ', entering)
+        print('People out: ', exiting)
+        print('count: ', count_QR)
     # In case there is not any scan QR 
     if myData == None:
         system_state = 5
@@ -340,88 +402,12 @@ def QRcheck():
         t3 = threading.Thread(target=buzzer_timeout)
         t3.start()
 
-# Function for Distance dectect
-def measure():
-    distance_detect = distance.get_distance()
-    print('{} cm'.format(distance_detect))
-    return distance_detect
-# Funtion for motion sensor dectected
-def on_detect():
-    global time_motion 
-    global flag                                                             # global variable
-    global count_QR
-    global exiting
-    global total
-    global entering
-    global system_state
-    global distant_count                                                    
-    global lcd_state
-    global current_distance
-    global pre_distance
-    if system_state == 3:                                                   # Invalid
-        system_state = 0
-        print('Invalidout')
-    elif system_state == 2:                                                 # Room full
-        system_state = 0
-        print('Room full')
-    elif system_state == 5:                                                 # No scan
-        system_state = 0
-        print('No scan')    
-    elif system_state == 0 or system_state == 1 or system_state == 4:
-        flag = 1
-        system_state = 0
-        sio.emit('checkout',system_state)
-        print('Motion detected')                                            # print out whenever motion detected
-        # turn on Ultra sonic sensor
-        while flag != 0:
-            distance_detect = measure()
-            if current_distance == 1:
-                pre_distance += current_distance
-            elif current_distance == 0:
-                pre_distance = 0
-            print('pre_distance', pre_distance)
-            # print('cur_distance', current_distance)
-            time.sleep(0.2)
-            if distance_detect < DISTANCE:                                 # if people in range -> run QR scan
-                current_distance = 1
-                distant_count += 1
-                if pre_distance == 4:
-                    distant_count = 0
-                    pre_distance = 0
-                    QRcheck()
-                    flag = 0
-            else:
-                current_distance = 0                                                           # in case people exit the room
-                time_motion += 1                                            # counting time for open ultra sonic
-                if time_motion == TIME_MOTION:                              # Time limit for run ultra sonic
-                    time_motion = 0
-                    if total == 0:
-                        exiting = exiting
-                    else:
-                        exiting += 1
-                        system_state = 4  
-                        lcd_state = 4
-                    flag = 0
-        total = entering - exiting                                          # total people in room
-        roomfull_on()
-        sio.emit('motion', {'total_people':total,  'people_in':entering, 'people_out':exiting})  # Send data inside room to UI
-        sio.emit('checkout',system_state)
-        count_QR = 0                                                        # Set time count back 0 for next loop
-
-# Function detection on motion
-# def motion_on():
-#     motionSensor.on_detect = on_detect 
 # main function
-def main():    
+def main():
+    t5 = threading.Thread(target=roomCondition)
+    t5.start()   
     while True:
         on_motion()
-        # roomCondition()
-        # t4 = threading.Thread(target=LCD_roomCondition)
-        # t4.start()
-        # on_motion()
-        # print('1')
-        # t5 = threading.Thread(target=motion_on)
-        # t5.start()                                                           # Motion on
  
 if __name__ == '__main__':
     main() 
