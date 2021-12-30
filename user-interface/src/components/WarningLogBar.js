44d9ab1adc0bd6a4dc2d6bc24915fc9c01429f21
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2021-12-30 18:02:09 +0700

    ADD user-interface

--- user-interface/src/components/WarningLogBar.js
+++ user-interface/src/components/WarningLogBar.js
@@ -1,52 +1,30 @@
 import Alert from "react-bootstrap/Alert";
 import socketIOClient from "socket.io-client";
 
+const ENTRANCE_PEOPLE = "entrance_name"
+
 const WarningLogBar = ({ url }) => {
   const date = new Date();
-  const warningMessage = "Social distance violation detected!";
-
-  // constant variable 
-  const ALARM_SOCIAL_DISTANCE = "alarm_distance"
-  const SAFE_SOCIAL_DISTANCE = "safe_distance"
-  const ALARM_PEOPLE_GATHERING = "alarm_gathering"
-  const SAFE_PEOPLE_GATHERING = "safe_gathering"
-
-  // HEADER socket received 
-  const SOCIAL_DISTANCE_SOCKET = "socialDistance"
-  const PEOPLE_GATHERING_SOCKET = "peopleGathering"
+  //List people 
+  var queue = [];
 
   // Socket 
-  const ENDPOINT = `${url}:5000`;
+  const ENDPOINT = `${url}:4000`;
   const socket = socketIOClient(ENDPOINT);
 
-  // Socket send social distance 
-  socket.on(SOCIAL_DISTANCE_SOCKET, function (social_distance_data) {
-    if(social_distance_data ===  ALARM_SOCIAL_DISTANCE){
-      // Warning for social distance
-      document.querySelector(".social_distance_alarm").style.filter = "brightness(100%)";
-      document.querySelector(".social_distance_safe").style.filter = "brightness(50%)";
+  socket.on(ENTRANCE_PEOPLE, function (data_name) {
+    // push to queue name
+    if(queue.length != 1){
+      queue.shift();  //put 
     }
+    queue.push(data_name);
 
-    else if (social_distance_data ===  SAFE_SOCIAL_DISTANCE){
-      // safe for social distance 
-      document.querySelector(".social_distance_alarm").style.filter = "brightness(50%)";
-      document.querySelector(".social_distance_safe").style.filter = "brightness(100%)";
-    }
-    // console.log(social_distance_data);
-  });
-
-  // Socket send social distance 
-  socket.on(PEOPLE_GATHERING_SOCKET, function (people_gathering_data) {
-    if(people_gathering_data ===  ALARM_PEOPLE_GATHERING){
-      // Warning for people gathering
-      document.querySelector(".people_gathering_alarm").style.filter = "brightness(100%)";
-      document.querySelector(".people_gathering_safe").style.filter = "brightness(50%)";
-    }
-
-    else if (people_gathering_data ===  SAFE_PEOPLE_GATHERING){
-      // safe for people gathering 
-      document.querySelector(".people_gathering_alarm").style.filter = "brightness(50%)";
-      document.querySelector(".people_gathering_safe").style.filter = "brightness(100%)";
+    switch(queue.length){
+      case 1:
+        document.getElementById("name_id_1").innerHTML = queue[0] + " has entered the room"
+      case 2:
+        document.getElementById("name_id_1").innerHTML = queue[0] + " has entered the room"
+        document.getElementById("name_id_2").innerHTML = queue[1] + " has entered the room"   
     }
   });
 
@@ -62,48 +40,25 @@ const WarningLogBar = ({ url }) => {
     // </Alert>
     <div className="border shadow-sm p-2 rounded bg-dark m-2">
       <div className="row">
-        <div className="col-2">
-          {/* soical distance  alarm*/}
-          <img
-            className="social_distance_alarm"
-            src="./UI_Vision/social_distance_alarm.jpg"
-            alt="social distance alarm"
-            style={{ objectFit: "cover", maxWidth: "10vw", maxHeight: "10vh" }}
-          ></img>
+        <div className="col-6 text-white">
+          <h4>{date.toString()}</h4>
         </div>
-        <div className="col-2">
-          {/* soical distance  safe*/}
-          <img
-            className="social_distance_safe"
-            src="./UI_Vision/social_distance_safe.jpg"
-            alt="social distance safe"
-            style={{ objectFit: "cover", maxWidth: "10vw", maxHeight: "10vh" }}
-          ></img>
-        </div>
-
-        <div className="col-2">
-          {/* people gathering alarm */}
-          <img
-            className="people_gathering_alarm"
-            src="./UI_Vision/people_gathering_alarm.png"
-            alt="people gathering alarm"
-            style={{ objectFit: "cover", maxWidth: "10vw", maxHeight: "10vh" }}
-          ></img>
-        </div>
-        <div className="col-2">
-          {/* people gathering safe*/}
-          <img
-            className="people_gathering_safe"
-            src="./UI_Vision/people_gathering_safe.png"
-            alt="people gathering safe"
-            style={{ objectFit: "cover", maxWidth: "10vw", maxHeight: "10vh" }}
-          ></img>
-        </div>
-
-        <div className="col-4 text-white">
-          {date.toString()}
-        </div>
+        <div className="col-6 " 
+              style={{
+                fontSize:"1.5rem",
+                fontWeight:"bold",
+              }}>
+          {/* <h4 id="name1_id">Han has entered the room</h4>
+          <h4 id="name2_id">Duc has entered the room</h4> */}
+          <ul className="list-group">
+            <li id="name_id_1" className="list-group-item disabled">Han has entered the room</li>
+            <li id="name_id_2" className="list-group-item">Dud has entered the room</li>
+          </ul>
       </div>
+      
+    </div>
+
+      
     </div>
     
   );
